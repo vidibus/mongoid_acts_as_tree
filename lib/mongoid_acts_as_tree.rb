41fcd240c3dbc687660815164bee32f6ba018c6e
saks <saksmlz@gmail.com>
2010-05-04 10:33:10 +0300

    formatting

--- lib/mongoid_acts_as_tree.rb
+++ lib/mongoid_acts_as_tree.rb
@@ -1,171 +1,171 @@
 require "mongoid"
 
 module Mongoid
-  module Acts
-    module Tree
-      def self.included(model)
-        model.class_eval do
-          extend InitializerMethods
-        end
-      end
+	module Acts
+		module Tree
+			def self.included(model)
+				model.class_eval do
+					extend InitializerMethods
+				end
+			end
 
-      module InitializerMethods
-        def acts_as_tree(options = {})
-          options = {
-            :parent_id_field => "parent_id",
-            :path_field      => "path",
-            :depth_field     => "depth"
-          }.merge(options)
+			module InitializerMethods
+				def acts_as_tree(options = {})
+					options = {
+						:parent_id_field => "parent_id",
+						:path_field      => "path",
+						:depth_field     => "depth"
+					}.merge(options)
 
-          write_inheritable_attribute :acts_as_tree_options, options
-          class_inheritable_reader :acts_as_tree_options
+					write_inheritable_attribute :acts_as_tree_options, options
+					class_inheritable_reader :acts_as_tree_options
 
-          include InstanceMethods
-          include Fields
-          extend Fields
-          extend ClassMethods
+					include InstanceMethods
+					include Fields
+					extend Fields
+					extend ClassMethods
 
-          field parent_id_field, :type => BSON::ObjectID
-          field path_field, :type => Array,  :default => [], :index => true
-          field depth_field, :type => Integer, :default => 0
+					field parent_id_field, :type => BSON::ObjectID
+					field path_field, :type => Array,  :default => [], :index => true
+					field depth_field, :type => Integer, :default => 0
 
-          after_save      :move_children
-          validate        :will_save_tree
-          before_destroy  :destroy_descendants
-        end
-      end
+					after_save      :move_children
+					validate        :will_save_tree
+					before_destroy  :destroy_descendants
+				end
+			end
 
-      module ClassMethods
-        def roots
-        	self.where(parent_id_field => nil).order_by tree_order
-        end
-      end
+			module ClassMethods
+				def roots
+					self.where(parent_id_field => nil).order_by tree_order
+				end
+			end
 
-      module InstanceMethods
-      	def [](field_name)
-      		self.send field_name
-      	end
+			module InstanceMethods
+				def [](field_name)
+					self.send field_name
+				end
 
-      	def []=(field_name, value)
-      		self.send "#{field_name}=", value
-      	end
+				def []=(field_name, value)
+					self.send "#{field_name}=", value
+				end
 
-        def ==(other)
-          return true if other.equal?(self)
-          return true if other.instance_of?(self.class) and other._id == self._id
-          false
-        end
+				def ==(other)
+					return true if other.equal?(self)
+					return true if other.instance_of?(self.class) and other._id == self._id
+					false
+				end
 
-        def will_save_tree
-          if @_cyclic
-            errors.add(:base, "Can't be children of a descendant")
-          end
-        end
+				def will_save_tree
+					if @_cyclic
+						errors.add(:base, "Can't be children of a descendant")
+					end
+				end
 
-        def fix_position
-          if parent.nil?
-            self[parent_id_field] = nil
-            self[path_field] = []
-            self[depth_field] = 0
-          else
-            self[parent_id_field] = parent._id
-            self[path_field] = parent[path_field] + [parent._id]
-            self[depth_field] = parent[depth_field] + 1
-            self.save
-          end
-        end
+				def fix_position
+					if parent.nil?
+						self[parent_id_field] = nil
+						self[path_field] = []
+						self[depth_field] = 0
+					else
+						self[parent_id_field] = parent._id
+						self[path_field] = parent[path_field] + [parent._id]
+						self[depth_field] = parent[depth_field] + 1
+						self.save
+					end
+				end
 
-        def parent
-          @_parent or (self[parent_id_field].nil? ? nil : self.class.find(self[parent_id_field]))
-        end
+				def parent
+					@_parent or (self[parent_id_field].nil? ? nil : self.class.find(self[parent_id_field]))
+				end
 
-        def root?
-          self[parent_id_field].nil?
-        end
+				def root?
+					self[parent_id_field].nil?
+				end
 
-        def root
-          self[path_field].first.nil? ? self : self.class.find(self[path_field].first)
-        end
+				def root
+					self[path_field].first.nil? ? self : self.class.find(self[path_field].first)
+				end
 
-        def ancestors
-          return [] if root?
-          self.class.find(self[path_field])
-        end
+				def ancestors
+					return [] if root?
+					self.class.find(self[path_field])
+				end
 
-        def self_and_ancestors
-          ancestors << self
-        end
+				def self_and_ancestors
+					ancestors << self
+				end
 
-        def siblings
-          self.class.where(:_id.ne => self._id, parent_id_field => self[parent_id_field]).order_by tree_order
-        end
+				def siblings
+					self.class.where(:_id.ne => self._id, parent_id_field => self[parent_id_field]).order_by tree_order
+				end
 
-        def self_and_siblings
-          self.class.where(parent_id_field => self[parent_id_field]).order_by tree_order
-        end
+				def self_and_siblings
+					self.class.where(parent_id_field => self[parent_id_field]).order_by tree_order
+				end
 
-        def children
-        	Children.new self
-        end
+				def children
+					Children.new self
+				end
 
-        def children=(new_children_list)
-        	self.children.clear
-        	new_children_list.each do | child |
-        		self.children << child
-        	end
-        end
+				def children=(new_children_list)
+					self.children.clear
+					new_children_list.each do | child |
+						self.children << child
+					end
+				end
 
 				alias replace children=
 
-        def descendants
-          return [] if new_record?
-          self.class.where(path_field => self._id).order_by tree_order
-        end
+				def descendants
+					return [] if new_record?
+					self.class.where(path_field => self._id).order_by tree_order
+				end
 
-        def self_and_descendants
-          [self] + self.descendants
-        end
+				def self_and_descendants
+					[self] + self.descendants
+				end
 
-        def is_ancestor_of?(other)
-          other[path_field].include?(self._id)
-        end
+				def is_ancestor_of?(other)
+					other[path_field].include?(self._id)
+				end
 
-        def is_or_is_ancestor_of?(other)
-          (other == self) or is_ancestor_of?(other)
-        end
+				def is_or_is_ancestor_of?(other)
+					(other == self) or is_ancestor_of?(other)
+				end
 
-        def is_descendant_of?(other)
-          self[path_field].include?(other._id)
-        end
+				def is_descendant_of?(other)
+					self[path_field].include?(other._id)
+				end
 
-        def is_or_is_descendant_of?(other)
-          (other == self) or is_descendant_of?(other)
-        end
+				def is_or_is_descendant_of?(other)
+					(other == self) or is_descendant_of?(other)
+				end
 
-        def is_sibling_of?(other)
-          (other != self) and (other[parent_id_field] == self[parent_id_field])
-        end
+				def is_sibling_of?(other)
+					(other != self) and (other[parent_id_field] == self[parent_id_field])
+				end
 
-        def is_or_is_sibling_of?(other)
-          (other == self) or is_sibling_of?(other)
-        end
+				def is_or_is_sibling_of?(other)
+					(other == self) or is_sibling_of?(other)
+				end
 
-        def move_children
+				def move_children
 
-          if @_will_move
-            @_will_move = false
-            self.children.each do | child |
-              child.fix_position
-              child.save
-            end
-            @_will_move = true
-          end
-        end
+					if @_will_move
+						@_will_move = false
+						self.children.each do | child |
+							child.fix_position
+							child.save
+						end
+						@_will_move = true
+					end
+				end
 
-        def destroy_descendants
-          self.descendants.each &:destroy
-        end
-      end
+				def destroy_descendants
+					self.descendants.each &:destroy
+				end
+			end
 
 			#proxy class
 			class Children < Array
@@ -181,11 +181,11 @@ module Mongoid
 					if object.descendants.include? @parent
 						object.instance_variable_set :@_cyclic, true
 					else
-	          object[object.parent_id_field] = @parent._id
-	          object[object.path_field] = @parent[@parent.path_field] + [@parent._id]
-	          object[object.depth_field] = @parent[@parent.depth_field] + 1
+						object[object.parent_id_field] = @parent._id
+						object[object.path_field] = @parent[@parent.path_field] + [@parent._id]
+						object[object.depth_field] = @parent[@parent.depth_field] + 1
 						object.instance_variable_set :@_will_move, true
-	          object.save
+						object.save
 					end
 
 					super(object)
@@ -225,24 +225,24 @@ module Mongoid
 
 			end
 
-      module Fields
-        def parent_id_field
-          acts_as_tree_options[:parent_id_field]
-        end
+			module Fields
+				def parent_id_field
+					acts_as_tree_options[:parent_id_field]
+				end
 
-        def path_field
-          acts_as_tree_options[:path_field]
-        end
+				def path_field
+					acts_as_tree_options[:path_field]
+				end
 
-        def depth_field
-          acts_as_tree_options[:depth_field]
-        end
+				def depth_field
+					acts_as_tree_options[:depth_field]
+				end
 
-        def tree_order
-          acts_as_tree_options[:order] or []
-        end
-      end
-    end
-  end
+				def tree_order
+					acts_as_tree_options[:order] or []
+				end
+			end
+		end
+	end
 end
 
